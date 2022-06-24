db0a22a939a71935bb38269adbb21bf071f0a749
Kévin Andrieux <kevin@katechconsult.fr>
2022-06-24 12:52:34 +0200

    Ajout d'une classe mere de consomation de l'API

---
+++ frontend/src/services/apiService.ts
@@ -0,0 +1,27 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+import userStore from "@/store/userStore";
+
+
+// Classe mère pour les services consomateurs de l'API
+export default class ApiService {
+    instance: AxiosInstance;
+
+    constructor() {
+        this.instance = axios.create({
+            baseURL: 'https://appli.docker.localhost/api',
+            timeout: 1000,
+            // headers: {'X-Custom-Header': 'foobar'}
+        });
+        // Add a request interceptor
+        this.instance.interceptors.request.use(function (config:AxiosRequestConfig) {
+            const token = userStore.state.token;
+            if (config.headers)
+            {
+                config.headers.Authorization = `Bearer ${token}`;   
+            }
+
+            return config;
+        });
+    }
+
+}
