bbedb87384c296b7b87bb9036826ecc5c2140805
Ines Touloum <ines.touloum@viacesi.fr>
2022-06-27 11:40:16 +0200

    correction webseckets + webpack mac os

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    client: {
+      webSocketURL: 'wss://appli.docker.localhost/ws',
+    },
     allowedHosts: "all"
   }
 })
