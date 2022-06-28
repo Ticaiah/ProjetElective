3d8da9ff4f7ac2d2c6e0a9e25cc1763e6dc1e84f
Kévin Andrieux <kevin@katechconsult.fr>
2022-06-28 15:56:00 +0200

    get token from cookie

--- frontend/src/services/apiService.ts
+++ frontend/src/services/apiService.ts
@@ -1,11 +1,11 @@
+import CookieUtils from "@/utils/cookieUtils";
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
-
 
 // Classe mère pour les services consomateurs de l'API
 export default class ApiService {
     instance: AxiosInstance;
 
-    constructor(token: string) {
+    constructor() {
         this.instance = axios.create({
             baseURL: 'https://appli.docker.localhost/api',
             timeout: 1000,
@@ -13,7 +13,7 @@ export default class ApiService {
         });
         // Add a request interceptor
         this.instance.interceptors.request.use(function (config:AxiosRequestConfig) {
-
+            var token = CookieUtils.getCookie("token");
             if (config.headers)
             {
                 config.headers.Authorization = `Bearer ${token}`;   
