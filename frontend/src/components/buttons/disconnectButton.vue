690185449f94a98fd024e211741ebc678c77f833
Ines Touloum <ines.touloum@viacesi.fr>
2022-06-29 10:18:25 +0200

    du jolie pour la petite carte compte sur la navbar

--- frontend/src/components/buttons/disconnectButton.vue
+++ frontend/src/components/buttons/disconnectButton.vue
@@ -1,20 +1,22 @@
 <template>
   <v-menu bottom min-width="200px" rounded offset-y>
     <template v-slot:activator="{ on }">
-      <v-btn icon x-large v-on="on">
-        <v-avatar color="brown" size="48">
-          <span class="white--text text-h5">IT</span>
-        </v-avatar>
-      </v-btn>
+      <div>
+        <v-btn icon class="avatar" x-large v-on="on">
+          <v-avatar  color="green darken-1" size="48">
+            <span class="white--text text-h5"><v-icon>mdi-account</v-icon></span>
+          </v-avatar>
+        </v-btn>
+      </div>  
     </template>
     <v-card>
       <v-list-item-content class="justify-center">
         <div class="mx-auto text-center">
-          <v-avatar color="brown">
-            <span class="white--text text-h5">IT</span>
+          <v-avatar color="green darken-1">
+            <span class="white--text text-h5"><v-icon color="white">mdi-account</v-icon></span>
           </v-avatar>
-          <h3>Ines Touloum</h3>
-          <p class="text-caption mt-1">oui@oui.fr</p>
+          <h3>{{name}}</h3>
+          <p class="text-caption mt-1">{{mail}}</p>
           <v-divider class="my-3"></v-divider>
           <v-btn depressed rounded text to="/my-account"> Mon compte </v-btn>
           <v-divider class="my-3"></v-divider>
@@ -31,6 +33,10 @@ import CookieUtils from "@/utils/cookieUtils";
 
 @Component
 export default class DisconnectButton extends Vue {
+
+  mail = this.$store.state.userStore.auth.mail;
+  name = this.$store.state.userStore.auth.last_name +" "+this.$store.state.userStore.auth.first_name;
+
   public disconnect() {
     CookieUtils.deleteCookie("token");
     this.$store.dispatch("disconnectUser");
@@ -40,6 +46,9 @@ export default class DisconnectButton extends Vue {
 </script>
 
 <style>
+.avatar{
+  margin-right: 100px;
+}
 #connexion {
   margin: 5px;
 }
